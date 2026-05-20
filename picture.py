779804d43e738123fcf1ee7d7540c720a99e8a63
hnguyen29son <hnguyen29@ssis.edu.vn>
2026-05-20 09:01:00 +0700

    added blue

--- picture.py
+++ picture.py
@@ -27,7 +27,11 @@ def draw_picture(width, height):
         
     
     # code for BLUE triangles
+    sg.set_fill_color(colors[2]) # set fill color to red
     
+    for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
+        sg.fill_triangle(2*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 2*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT) 
+       
     
     # code for YELLOW triangles
    
@@ -46,3 +50,4 @@ def draw_picture(width, height):
 if __name__ == "__main__":
     # Launch the wrapper; only edit starting dimensions of canvas if you would like to
     sg.start(draw_picture, 600, 400)
+
